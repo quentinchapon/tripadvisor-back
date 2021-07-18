dd3151e4533b478fa7ffb8e90f6ca7959137fbee
Quentin Chapon <q.chapon@gmail.com>
2021-07-18 19:47:20 +0200

    Testing Mailgun

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(formidable());
 app.use(cors());
 
-const API_KEY = process.env.API_MAILGUN;
-const DOMAIN = process.env.DOMAIN_MAILGUN;
+const API_KEY = "API_MAILGUN";
+const DOMAIN = "DOMAIN_MAILGUN";
 const mailgun = require("mailgun-js")({
   host: "api.eu.mailgun.net",
   apiKey: API_KEY,
